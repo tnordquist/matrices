6b970aac0989a7f8b1c1f63ed0a1949fce438b88
Todd A Nordquist <toddnordquist144@gmail.com>
2019-03-10 20:48:46 -0600

    Matrices with @MethodSource test.  It seems to work for rotate but with rotateInPlace it keeps saying the input is void.

--- test/edu/cnm/deepdive/MatricesTest.java
+++ test/edu/cnm/deepdive/MatricesTest.java
@@ -2,8 +2,15 @@ package edu.cnm.deepdive;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+@DisplayName("Should pass the method parameters provided by the rotate() & "
+    + "rotateInPlace methods")
 class MatricesTest {
 
   private static final int[][] INPUT_ARR_3 = {{2, 4, 6}, {8, 10, 12},
@@ -15,22 +22,39 @@ class MatricesTest {
   private static final int[][] INPUT_ARR_1 = {{-1}};
   private static final int[][] EXPECTED_ARR_1 = {{-1}};
 
+  @DisplayName("Should calculate the correct 2d array")
+  @ParameterizedTest(name = "{index} => input={0}, output={1}")
+  @MethodSource("rotate")
+  static void rotate(int[][] input, int[][] output) {
+    assertEquals(output, Matrices.rotate(input));
+  }
 
-  @Test
-  void rotate() {
+  private static Stream<Arguments> rotate() {
+    return Stream.of(
+        Arguments.of(INPUT_ARR_1, EXPECTED_ARR_1),
+        Arguments.of(INPUT_ARR_2, EXPECTED_ARR_2),
+        Arguments.of(INPUT_ARR_3, EXPECTED_ARR_3)
 
-    assertArrayEquals(EXPECTED_ARR_1, Matrices.rotate(INPUT_ARR_1));
-    assertArrayEquals(EXPECTED_ARR_2, Matrices.rotate(INPUT_ARR_2));
-    assertArrayEquals(EXPECTED_ARR_3, Matrices.rotate(INPUT_ARR_3));
+    );
 
   }
 
-  @Test
-  void rotateInPlace() {
 
-    assertArrayEquals(EXPECTED_ARR_1, Matrices.rotate(INPUT_ARR_1));
-    assertArrayEquals(EXPECTED_ARR_2, Matrices.rotate(INPUT_ARR_2));
-    assertArrayEquals(EXPECTED_ARR_3, Matrices.rotate(INPUT_ARR_3));
 
-  }
+//  @DisplayName("Should calculate the correct 2d array")
+//  @ParameterizedTest(name = "{index} => input={0}, output={1}")
+//  @MethodSource("rotateInPlace")
+//  static void rotateInPlace(int[][] input, int[][] output) {
+//    assertEquals(output, Matrices.rotateInPlace(input));
+//  }
+//
+//  private static Stream<Arguments> rotateInPlace() {
+//    return Stream.of(
+//        Arguments.of(INPUT_ARR_1, EXPECTED_ARR_1),
+//        Arguments.of(INPUT_ARR_2, EXPECTED_ARR_2),
+//        Arguments.of(INPUT_ARR_3, EXPECTED_ARR_3)
+//
+//    );
+//
+//  }
 }
